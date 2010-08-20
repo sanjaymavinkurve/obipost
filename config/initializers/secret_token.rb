a94a3ed0f62569c3b1934e29f5853a89e29d6fe5
Sanjay Mavinkurve <sanjay@gmail.com>
2010-08-20 11:51:43 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Obipost::Application.config.secret_token = '7a6d0781cb271b55515e2f9e73eb4d4f76527fc1440dcbb04ab5e279992eac51f537042ebb29068ab6ceb25d48e768b8205c61f6a070e8253c8aae6bdaea565d'
