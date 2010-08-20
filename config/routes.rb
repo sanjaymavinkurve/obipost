72d9d1243ea0da57600c65956a2fad432425448d
Sanjay Mavinkurve <sanjay@gmail.com>
2010-08-20 16:44:32 -0700

    Finished layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Obipost::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
-
-  get "pages/about"
-
-  get "pages/faq"
+  get "users/new"
+  
+  match '/signup',  :to => 'users#new'
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/faq',    :to => 'pages#faq'
+  
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -53,10 +55,6 @@ Obipost::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
