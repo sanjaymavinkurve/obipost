e9cdb474127ae72356982351d2ef5182875a109b
Sanjay Mavinkurve <sanjay@gmail.com>
2010-09-03 01:10:09 -0700

    S3 configuration - take 1

--- app/models/photoframe.rb
+++ app/models/photoframe.rb
@@ -1,5 +1,8 @@
 class Photoframe < ActiveRecord::Base
   belongs_to :post
 
-  has_attached_file :photo
+  has_attached_file :photo,
+    :storage => :s3, 
+    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", 
+    :path => "/:style/:filename"
 end
