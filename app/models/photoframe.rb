3dcf12a6ee7a9adf6d87db258456093de33387e6
Sanjay Mavinkurve <sanjay@gmail.com>
2010-09-03 15:45:47 -0700

    x

--- app/models/photoframe.rb
+++ app/models/photoframe.rb
@@ -2,7 +2,7 @@ class Photoframe < ActiveRecord::Base
   belongs_to :post
 
   has_attached_file :photo,
-    :storage => :s3, 
-    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", 
+    :storage => :s3,
+    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
     :path => "/:style/:filename"
 end
