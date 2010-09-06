c04a350ce7d0e5a4aaa822c90db0368466660c2a
Sanjay Mavinkurve <sanjay@gmail.com>
2010-09-05 22:33:02 -0700

    x

---
+++ db/migrate/20100906044415_add_unfinished_to_posts.rb
@@ -0,0 +1,11 @@
+class AddUnfinishedToPosts < ActiveRecord::Migration
+  def self.up
+    add_column :posts, :completed, :boolean
+    add_column :posts, :disabled, :boolean
+  end
+
+  def self.down
+    remove_column :posts, :disabled
+    remove_column :posts, :completed
+  end
+end
